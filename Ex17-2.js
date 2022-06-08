53158137b22dcea7ef83561d4ffde755d21f7e57
gaoey <gaoey@hotmail.com>
2022-06-08 23:11:38 +0700

    feat: comment

--- Ex17-2.js
+++ Ex17-2.js
@@ -14,10 +14,17 @@ function calculate_recursive(numb, total, current_index, sum) {
     let digit = Math.pow(10, current_index);
     let new_sum = numb * digit + sum;
     return (
-      new_sum + calculate_recursive(numb, total, current_index + 1, new_sum)
+      new_sum - calculate_recursive(numb, total, current_index + 1, new_sum)
     );
   }
 }
+
+// new_sum
+// 2 + calculate_recursive(2, 5, 1, 2);
+// 22 + calculate_recursive(2, 5, 2, 22);
+// 222 + calculate_recursive(2, 5, 3, 222);
+// 2222 + calculate_recursive(2, 5, 4, 2222);
+// 22222 + calculate_recursive(2, 5, 5, 22222);
 
 // unit test
 console.log("2 + 22 + 222 + 2222 + 22222 = 24690 is ", summation(2, 5));
